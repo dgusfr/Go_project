da3c6c79a56919f9bef86c630c62715304bb50c0
dgusfr <diegogustavo.fr@gmail.com>
2024-11-30 18:44:39 -0300

    feat(add): tuype of

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	var nome string = "Diego"
 	var idade int = 27
 	var altura float32 = 1.82
-	
+
   fmt.Println("Olá,", nome) 
-	fmt.Println("Sua idade é de :", idade)
-	fmt.Println("Sua altura é de :", altura)
+	fmt.Println("Sua idade é de :", idade, "e sua altura:", altura)
+
+	fmt.Println("O tipo da variavél nome é:", reflect.TypeOf((nome)))
 }
