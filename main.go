9645e810bc93e3c9989f8a111887e828e55914b6
dgusfr <diegogustavo.fr@gmail.com>
2024-11-30 19:05:41 -0300

    feat(add): exibe introdução

--- main.go
+++ main.go
@@ -6,15 +6,7 @@ import (
 )
 
 func main() {
-	// var nome string = "Diego" it's equal to:
-	nome := "Diego"
-	idade := 27
-	altura := 1.82
-
-  fmt.Println("Olá,", nome) 
-	fmt.Println("Sua idade é de :", idade, "e sua altura:", altura)
-
-	fmt.Println("O tipo da iavél nome é:", reflect.TypeOf((nome)))
+	exibeIntroducao()
 
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir  Logs")
@@ -46,3 +38,17 @@ func main() {
 		fmt.Println("Comando incorreto")
 	}
 }
+
+func exibeIntroducao(){
+	// var nome string = "Diego" it's equal to:
+	nome := "Diego"
+	idade := 27
+	altura := 1.82
+
+  fmt.Println("Olá,", nome) 
+	fmt.Println("Sua idade é de :", idade, "e sua altura:", altura)
+
+	fmt.Println("O tipo da iavél nome é:", reflect.TypeOf((nome)))
+}
+
+func
