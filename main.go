ff1fa13eb78c155ae8e5fb1d48106722ae4aff7a
dgusfr <diegogustavo.fr@gmail.com>
2024-11-30 18:42:02 -0300

    feat(add): heigth in main

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	var nome string = "Diego"
-	var idade float32 = 27
+	var idade int = 27
+	var altura float32 = 1.82
+	
   fmt.Println("Olá,", nome) 
 	fmt.Println("Sua idade é de :", idade)
+	fmt.Println("Sua altura é de :", altura)
 }
